5c15292d763c8200a08c8ac577d2b157294c42a7
Sam-Guliker <samguliker@hotmail.com>
2018-05-23 10:35:30 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,94 @@
-# drumkit
+## Opdracht 3 - Progressive Enhanced Browser Technologies
+![example](images/example.gif)
+## Core functionality
+The core functionality of my page will be to play sounds
+through the web browser.
+
+## Enhancement
+When you can run javascript you've got the power to play a drumkit.  
+The drumkit allows you to make a beat however you want! :sunglasses:
+
+## Feature detections
+I've used most of the mast basic javascript to prevent the feature   
+issues by browsers. I used some of the most basic JS functions that work in  
+every browser no matter what version or what browser there is.
+
+### Feature detection
+Will check if the `addEventListener` is accessible in the document.  
+If it isn't in the document use the attachEvent.
+```Javascript
+function onClickEvent(el, item){
+  if (document.addEventListener) {
+    el.addEventListener('keydown', item);
+  } else {
+    el.attachEvent('keydown', item);
+  }
+}
+```
+
+### ES6 to ES5
+Es6 using const and arrowkeys
+```Javascript
+const keys = document.querySelectorAll('.key');
+const audio =
+document.querySelector(`audio[data-key="${e.keyCode}"]`);
+const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+keys.forEach(key => key.addEventListener('click', playSound))
+
+
+```
+
+Es5 instead of using arrow and for each functions I used for loops.  
+
+```Javascript
+var keys = document.querySelectorAll('.key');
+var audio = document.querySelector("audio[data-key=" + "'" + keycode + "'" + "]");
+var key = document.querySelector(".key[data-key=" + "'" + keycode + "'"+ "]");
+
+for (var x = 0; x < keys.length; x++) {
+  keys[x].addEventListener('transitionend', removeTransition)
+}
+
+for (var i = 0; i < keys.length; i++){
+  keys[i].addEventListener('click', playSound)
+}
+
+
+
+```
+## Feature support
+
+### Audio Element
+The audio element is a strong element to use, it got great support.
+
+![audioelement](images/audioelement.png)
+
+### ES5
+I've written all my code in ES5 so the browsers that can be supported will get the good ol' javascript :wink:.
+
+#### Support
+
+![es5](images/es5.png)
+
+### DOM Manipulation.
+To make sure I was using the most supported code for javascript I'm using code like `getElementsByTagName`.
+
+#### Support
+
+![dom](images/dom.png)
+
+
+## Accessibility
+The user can use the tabs to navigate if he or she has to.
+When the javascript works there is no reason to use tabs, you
+can enjoy the enhancement! :smile:
+
+## Colour and contrast
+Chrome also has its own contrast checker.  
+I first used `F16562` as an highlight colour, but I found out  
+that it doesn't have enough contrast with my background.
+I used `#FF726F` instead.
+
+![color contrast](images/colorcontrast.png)
---
+++ public/js/script.js
@@ -0,0 +1,131 @@
+// Main function
+var keys = document.querySelectorAll('.key');
+var img = document.getElementsByTagName('img')[0]
+var html = document.getElementsByTagName('html')[0]
+var h1 = document.getElementsByTagName('h1')[0]
+
+
+function removeAudioControlls() {
+  var audioControls = document.getElementsByTagName('audio')
+  var f
+
+  for (f = 0; f < audioControls.length; f++) {
+    audioControls[f].controls = false;
+  }
+}
+removeAudioControlls()
+
+function playSound(e) {
+  //http://codeartists.com/post/36746402258/how-to-record-audio-in-chrome-with-native-html5
+  var keycode = e.keyCode ? e.keyCode : e.target.getAttribute('data-key')
+  var audio = document.querySelector("audio[data-key=" + "'" + keycode + "'" + "]");
+  var key = document.querySelector(".key[data-key=" + "'" + keycode + "'" + "]");
+
+  if (!audio) return; // stopt de functie
+
+  audio.currentTime = 0; // rewind to the start
+  audio.play();
+
+  key.classList.add('playing')
+};
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return; // skip if it's not a transform
+  this.classList.remove('playing')
+}
+
+for (var x = 0; x < keys.length; x++) {
+  keys[x].addEventListener('transitionend', removeTransition)
+}
+
+for (var i = 0; i < keys.length; i++) {
+  keys[i].addEventListener('click', playSound)
+  keys[i].addEventListener('focus', playSound)
+}
+
+window.addEventListener('keydown', playSound);
+
+// Enhancement
+if (AudioContext && navigator.mediaDevices.getuserMedia in window) {
+  audioCheck()
+}
+
+function audioCheck() {
+  var video = document.querySelector('video');
+  var main, recordButton, stopButton, playButton, pauseButton, canvas, audioContext, playBack, div, recording, isRecording
+
+  main = document.querySelector('main')
+  recordButton = document.createElement('button')
+  stopButton = document.createElement('button')
+  playBack = document.createElement('section')
+  div = document.createElement('div')
+  recording = document.querySelector('.record')
+  isRecording = false
+
+  audioContext = new(window.AudioContext || webkitAudioContext)()
+
+  // var canvasCtx = canvas.getContext("2d")
+  var constraints = {
+    audio: true,
+    video: false
+  }
+
+  var chunks = [];
+
+  recordButton.innerHTML = 'record'
+  recordButton.setAttribute('class', 'record')
+  stopButton.innerHTML = 'stop'
+  stopButton.setAttribute('class', 'stopRecord')
+  
+  main.append(div)
+  div.append(recordButton, stopButton, playBack)
+  recordButton.addEventListener('click', startRecording)
+
+  function startRecording() {
+    if(isRecording == true){
+      return
+    }
+    isRecording = true
+    recordButton.classList.add('active')
+    navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
+  }
+
+  // stopButton.addEventListener('click', stopRecording)
+
+  var onSuccess = function(stream) {
+    // Thanks to mees. https://github.com/meesrutten/browser-technologies/blob/master/opdracht3/scripts/main.js
+    // Starts recording
+    console.log("I'm recording")
+    var mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.start()
+
+    stopButton.addEventListener('click', function() {
+      console.log("I'm stopping now")
+      mediaRecorder.stop()
+    })
+
+    mediaRecorder.onstop = function(e) {
+      isRecording = false
+      recordButton.classList.remove('active')
+      var clipName = prompt('Enter a name for your sound clip?', 'My unnamed clip');
+
+			var blob = new Blob(chunks, { 'type': 'audio/ogg; codecs=opus' });
+			chunks = [];
+			var audioURL = window.URL.createObjectURL(blob);
+
+      playBack.insertAdjacentHTML('beforeend', '<article class="clip" id="' + clipName + '"><h2>' + clipName + '</h2><audio controls="" src="' + audioURL + '"></audio></article>');
+
+    }
+    
+    mediaRecorder.ondataavailable = function(e) {
+      chunks.push(e.data)
+      console.log(chunks)
+    }
+  
+  }
+
+    var onError = function(err) {
+      console.log('The following error occured: ' + err);
+    }
+
+}
